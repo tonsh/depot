1ebcf048f9d03d60e9bcb67641921c7b84245fcb
tonsh <tonshlee@gmail.com>
2012-08-05 13:21:57 +0800

    Product validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ActiveRecord::Base
   attr_accessible :date_available, :description, :image_url, :price, :title
+
+  validates_presence_of :title, :image_url, :price
+  validates_numericality_of :price
+  validates_uniqueness_of :title
+
+  validates_format_of :image_url,
+                      :with => %r{^http:.+\.(gif|jpg|png)$}i,
+                      :message => "product_image_url_format_error"
+
+  protected
+  def validate
+    errors.add(:price, "product_price_should_be_positive") unless price.nil? || price >= 0.01
+  end
 end
