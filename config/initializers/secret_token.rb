d6513081bd292deaef96f59ca2d906f5ab138cfc
tonsh <tonshlee@gmail.com>
2012-08-05 10:26:47 +0800

    Install rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'db0a6ae875a3cc1e5ebcafe4b1d3366ebf5b6c8f65d64266eab9f90cf12f06b6df32b6f59fca5b92a0b4b64c2d7a7cda362d8750376aadb0abf509cdb4b79162'
