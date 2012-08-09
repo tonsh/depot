369c5f9d1aec82c4d1120a7e5e8ddd5148460390
tonsh <tonshlee@gmail.com>
2012-08-10 02:06:35 +0800

    Form表单及验证错误信息使用bootstrap样式

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module Depot
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # disable css by default
+    #config.generators do |g|
+    #  g.stylesheets false
+    #end
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| %Q(#{html_tag}).html_safe }
   end
 end
