429e690a76fe0260dcdd7371c9c0fe505b17637e
tonsh <tonshlee@gmail.com>
2012-08-11 02:03:25 +0800

    add LineItem migrate

--- db/migrate/20120810175303_create_line_items.rb
+++ db/migrate/20120810175303_create_line_items.rb
@@ -1,6 +1,9 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
+      t.integer :product_id, :null => false
+      t.integer :quantity, :null => false, :default => 0 
+      t.decimal :unit_price, :precision => 10, :scale =>2, :null => false
 
       t.timestamps
     end
