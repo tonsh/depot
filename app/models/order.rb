5b1cf9e57eb05f859675aa2ecdc895964a121c78
tonsh <tonshlee@gmail.com>
2012-08-12 01:35:17 +0800

    valid with order checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ActiveRecord::Base
   attr_accessible :name, :email, :address, :pay_type
   has_many :line_items
 
+  validates_presence_of :name, :email, :address, :pay_type
+
   PAYMENT_TYPES = [
     ["Check",           "check"],
     ["Credit Card",     "cc"],
